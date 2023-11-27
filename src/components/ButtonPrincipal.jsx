65e45da903666baf47db067341b0d8d2b0fdd7d3
Sohail Iran <aratavalga@gmail.com>
2023-11-26 19:26:14 -0500

    foodrepo tailwindcss

---
+++ src/components/ButtonPrincipal.jsx
@@ -0,0 +1,5 @@
+export const ButtonPrincipal = ({ name }) => {
+    return (
+        <button className="transition ease-in-out duration-300 delay-200 bg-persian_red text-white py-3 px-9 rounded-3xl font-semibold hover:bg-celadon hover:text-black">{name}</button>
+    )
+}
---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			"beige": "#E6E8D2",
+			"bittersweet_shimmer": "#B85043",
+			"persian_red": "#B6473A",
+			"eerie_black": "#181818",
+			"gavy's_gray": "#575757",
+			"celadon": "#7ED4A7",
+			"white": "#FFFFFF",
+			"black": "#000000"
+		},
+		extend: {},
+	},
+	plugins: [],
+}
